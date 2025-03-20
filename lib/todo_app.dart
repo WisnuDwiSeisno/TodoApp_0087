63638519ebbcb773d58a21d25840898b6462dcb5
WisnuDwiSeisno <wseisno37@gmail.com>
2025-03-21 01:41:03 +0700

    membuat data dummy dan membuat appbar

--- lib/todo_app.dart
+++ lib/todo_app.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TodoApp extends StatelessWidget {
@@ -5,7 +6,40 @@ class TodoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(home: FormPage());
   }
 }
 
+class FormPage extends StatefulWidget {
+  const FormPage({super.key});
+
+  @override
+  State<FormPage> createState() => _FormPageState();
+}
+
+class _FormPageState extends State<FormPage> {
+  final _formKey = GlobalKey<FormState>();
+  DateTime? _selectedDate;
+  String? _firstName;
+  bool _showDatePicker = false;
+
+  // Dummy data task
+  final List<Map<String, dynamic>> tasks = [
+    {'title': 'Makan', 'deadline': '15-03-2025 14:03', 'done': false},
+    {'title': 'Minum', 'deadline': '14-03-2025 12:03', 'done': true},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: const Icon(Icons.arrow_back_ios, color: Colors.black),
+        title: const Text('FORM PAGE', style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+        elevation: 0,
+      ),
+    );
+  }
+}
